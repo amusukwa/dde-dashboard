18741a82f2c05b1764540a3e2de7b89567d2f5f6
amusukwa <amusukwa@gmail.com>
2018-05-24 16:17:04 +0200

    worked on adding map to dashboard

--- src/App.js
+++ src/App.js
@@ -1,82 +1,60 @@
-import React, { Component } from "react"
-import NavBar from "./Nav/NavBar"
-import LineGraph from "./charts/LineGraph"
-import Table from "./Table/Table"
-import Map from "./charts/Map"
-import { SVGMap, Taiwan } from "react-svg-map"
-import "react-svg-map/lib/index.css"
+import React, { Component } from 'react'
+import NavBar from './Nav/NavBar'
+import LineGraph from './charts/LineGraph'
+import Table from './Table/Table'
+import Map from './charts/Map'
+import { SVGMap, Taiwan } from 'react-svg-map'
+import 'react-svg-map/lib/index.css'
 import {
-  ComposableMap,
-  ZoomableGroup,
-  Geographies,
-  Geography
-} from "react-simple-maps"
+	ComposableMap,
+	ZoomableGroup,
+	Geographies,
+	Geography
+} from 'react-simple-maps'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <div className="row mfl-tm-5">
-          <div className="col m3 l3">
-            <ComposableMap style={{ width: "100%", height: "100%" }}>
-              <ZoomableGroup zoom={4}>
-                <Geographies geography={"topjson/map.json"}>
-                  {(geographies, projection) =>
-                    geographies.map(geography => (
-                      <Geography
-                        style={{
-                          default: { fill: "#666" },
-                          hover: { fill: "#999" },
-                          pressed: { fill: "#000" }
-                        }}
-                        key={geography.id}
-                        geography={geography}
-                        projection={projection}
-                      />
-                    ))
-                  }
-                </Geographies>
-              </ZoomableGroup>
-            </ComposableMap>
-          </div>
-          <br />
-          <div className="col m9 l9 mfl-tm-5">
-            <div className="mfl-tm-5">
-              <div className="row">
-                <div className="col m3 l4 mfl-tm-5 ">
-                  <LineGraph />
-                </div>
-                <div className="col m3 l4 mfl-tm-10 ">
-                  <LineGraph />
-                </div>
-                <div className="col m3 l4 mfl-tm-10 ">
-                  <LineGraph />
-                </div>
-                <div className="row">
-                  <div className="col m3 l4 mfl-tm-5 ">
-                    <LineGraph />
-                  </div>
-                  <div className="col m3 l4 mfl-tm-10 ">
-                    <LineGraph />
-                  </div>
-                  <div className="col m3 l4 mfl-tm-10 ">
-                    <LineGraph />
-                  </div>
-                </div>
-                <br />
-                <div className="row">
-                  <div className="col m12 l12 mfl-tm-10 ">
-                    <Table />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<div className="row mfl-tm-5">
+					<div className="col m3 l3">
+						<Map />
+					</div>
+					<div className="col m9 l9 mfl-tm-5">
+						<div className="row">
+							<div className="col m4 l4 mfl-tm-5 ">
+								<LineGraph />
+							</div>
+							<div className="col m4 l4 mfl-tm-10 ">
+								<LineGraph />
+							</div>
+							<div className="col m4 l4 mfl-tm-10 ">
+								<LineGraph />
+							</div>
+						</div>
+						<div className="row">
+							<div className="col m3 l4 mfl-tm-5 ">
+								<LineGraph />
+							</div>
+							<div className="col m3 l4 mfl-tm-10 ">
+								<LineGraph />
+							</div>
+							<div className="col m3 l4 mfl-tm-10 ">
+								<LineGraph />
+							</div>
+						</div>
+						<br />
+						<div className="row">
+							<div className="col m12 l12 mfl-tm-10 ">
+								<Table />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App
--- src/charts/Map.js
+++ src/charts/Map.js
@@ -1,49 +1,84 @@
-import React, { Component } from "react"
-import { Card, CardTitle } from "react-materialize"
+import React, { Component } from 'react'
+import { Card, CardTitle } from 'react-materialize'
 import {
-  VictoryLine,
-  VictoryChart,
-  VictoryAxis,
-  VictoryTheme,
-  VictoryCursorContainer
-} from "victory"
-import { fetchData } from "../actions"
-import { connect } from "react-redux"
-import ReactDOM from "react-dom"
+	VictoryLine,
+	VictoryChart,
+	VictoryAxis,
+	VictoryTheme,
+	VictoryCursorContainer
+} from 'victory'
+import { fetchData } from '../actions'
+import { connect } from 'react-redux'
+import ReactDOM from 'react-dom'
 import {
-  ComposableMap,
-  ZoomableGroup,
-  Geographies,
-  Geography
-} from "react-simple-maps"
+	ComposableMap,
+	ZoomableGroup,
+	Geographies,
+	Geography
+} from 'react-simple-maps'
 
 class Map extends Component {
-  componentDidMount() {}
-  render() {
-    return (
-      <div>
-        <div className="">
-          <div className="col mfl-tm-5 ">
-            <ComposableMap>
-              <ZoomableGroup>
-                <Geographies geography={"../topjson/gadm36_MW1_O.json"}>
-                  {(geographies, projection) =>
-                    geographies.map(geography => (
-                      <Geography
-                        key={geography.id}
-                        geography={geography}
-                        projection={projection}
-                      />
-                    ))
-                  }
-                </Geographies>
-              </ZoomableGroup>
-            </ComposableMap>
-          </div>
-        </div>
-      </div>
-    )
-  }
+	constructor() {
+		super()
+
+		this.state = {
+			zoom: 5
+		}
+
+		this.handleZoomIn = this.handleZoomIn.bind(this)
+		this.handleZoomOut = this.handleZoomOut.bind(this)
+	}
+	handleZoomIn() {
+		this.setState({
+			zoom: this.state.zoom * 2
+		})
+	}
+	handleZoomOut() {
+		this.setState({
+			zoom: this.state.zoom / 2
+		})
+	}
+
+	componentDidMount() {}
+	render() {
+		return (
+			<div>
+				<div className="map-ratio-container">
+					<div className=" ">
+						<ComposableMap
+							style={{
+								width: '500%',
+								height: 3000,
+								margin: -1800,
+								top: 0,
+								bottom: 100
+							}}
+						>
+							<ZoomableGroup zoom={this.state.zoom}>
+								<Geographies geography={'topjson/map.json'}>
+									{(geographies, projection) =>
+										geographies.map(geography => (
+											<Geography
+												style={{
+													default: { fill: '#6666' },
+													hover: { fill: '#999' },
+													pressed: { fill: '#000' },
+													width: '100%'
+												}}
+												key={geography.id}
+												geography={geography}
+												projection={projection}
+											/>
+										))
+									}
+								</Geographies>
+							</ZoomableGroup>
+						</ComposableMap>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
 // const mapStateToProps = state => ({
